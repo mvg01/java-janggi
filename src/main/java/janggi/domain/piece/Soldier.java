5c7593d77fae51ab267ac2a74e702d9c120885dd
mvg01 <dkms625@naver.com>
2026-04-23 16:00:26 +0900

    refactor: Soldier 생성자에서 PieceAction 생성 로직을 static 메서드로 분리

--- src/main/java/janggi/domain/piece/Soldier.java
+++ src/main/java/janggi/domain/piece/Soldier.java
@@ -10,28 +10,30 @@ import java.util.List;
 
 public class Soldier extends AbstractPiece {
     private static final PieceType PIECE_TYPE = PieceType.SOLDIER;
-    private static final PieceAction RED_PIECE_ACTION;
-    private static final PieceAction BLUE_PIECE_ACTION;
+    private static final PieceAction RED_PIECE_ACTION = createPieceAction(TeamType.RED);
+    private static final PieceAction BLUE_PIECE_ACTION = createPieceAction(TeamType.BLUE);
 
-    static {
-        final List<MoveRule> redMovementStrategies = List.of(
-                new NormalMoveRule(new Movement(Direction.LEFT), 1),
-                new NormalMoveRule(new Movement(Direction.RIGHT), 1),
-                new NormalMoveRule(new Movement(Direction.DOWN), 1),
-                new PalaceMoveRule(Direction.DOWN_LEFT),
-                new PalaceMoveRule(Direction.DOWN_RIGHT));
+    public Soldier(TeamType teamType) {
+        super(teamType);
+    }
+
+    private static PieceAction createPieceAction(TeamType teamType) {
+        if (teamType == TeamType.RED) {
+            final List<MoveRule> redMovementStrategies = List.of(
+                    new NormalMoveRule(new Movement(Direction.LEFT), 1),
+                    new NormalMoveRule(new Movement(Direction.RIGHT), 1),
+                    new NormalMoveRule(new Movement(Direction.DOWN), 1),
+                    new PalaceMoveRule(Direction.DOWN_LEFT),
+                    new PalaceMoveRule(Direction.DOWN_RIGHT));
+            return new PieceAction(redMovementStrategies);
+        }
         final List<MoveRule> blueMovementStrategies = List.of(
                 new NormalMoveRule(new Movement(Direction.LEFT), 1),
                 new NormalMoveRule(new Movement(Direction.RIGHT), 1),
                 new NormalMoveRule(new Movement(Direction.UP), 1),
                 new PalaceMoveRule(Direction.UP_LEFT),
                 new PalaceMoveRule(Direction.UP_RIGHT));
-        RED_PIECE_ACTION = new PieceAction(redMovementStrategies);
-        BLUE_PIECE_ACTION = new PieceAction(blueMovementStrategies);
-    }
-
-    public Soldier(TeamType teamType) {
-        super(teamType);
+        return new PieceAction(blueMovementStrategies);
     }
 
     @Override
@@ -41,7 +43,7 @@ public class Soldier extends AbstractPiece {
 
     @Override
     protected PieceAction getPieceAction() {
-        if (this.teamType == TeamType.RED) {
+        if (teamType == TeamType.RED) {
             return RED_PIECE_ACTION;
         }
         return BLUE_PIECE_ACTION;
